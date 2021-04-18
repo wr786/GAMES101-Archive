841e147912878c6816c68e8ea7f37a84da98f98a
wr786 <wr786@pku.edu.cn>
2021-04-18 20:24:53 +0800

    Deployed in Windows

---
+++ Assignment1/code/cmake-build-debug/CMakeFiles/Rasterizer.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/wr786/Documents/GitHub/GAMES101-Archive/Assignment1/code/Triangle.cpp" "C:/Users/wr786/Documents/GitHub/GAMES101-Archive/Assignment1/code/cmake-build-debug/CMakeFiles/Rasterizer.dir/Triangle.cpp.obj"
+  "C:/Users/wr786/Documents/GitHub/GAMES101-Archive/Assignment1/code/main.cpp" "C:/Users/wr786/Documents/GitHub/GAMES101-Archive/Assignment1/code/cmake-build-debug/CMakeFiles/Rasterizer.dir/main.cpp.obj"
+  "C:/Users/wr786/Documents/GitHub/GAMES101-Archive/Assignment1/code/rasterizer.cpp" "C:/Users/wr786/Documents/GitHub/GAMES101-Archive/Assignment1/code/cmake-build-debug/CMakeFiles/Rasterizer.dir/rasterizer.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "C:/__libs__/Eigen3/include"
+  "C:/__libs__/opencv/sources/build/install/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ pa0/cmake-build-debug/CMakeFiles/Transformation.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/wr786/Documents/GitHub/GAMES101-Archive/pa0/main.cpp" "C:/Users/wr786/Documents/GitHub/GAMES101-Archive/pa0/cmake-build-debug/CMakeFiles/Transformation.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "C:/__libs__/Eigen3/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
